2b6eadff265346eddfadb18deb7e01c2adab7a81
Jjav2 <Jjav2@HPCOMPUTER18>
2024-05-14 18:10:39 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FileListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnOpen = new Button();
+            lstvArchive = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            btnOpen.Location = new Point(36, 48);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(126, 82);
+            btnOpen.TabIndex = 0;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // lstvArchive
+            // 
+            lstvArchive.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            lstvArchive.Location = new Point(242, 24);
+            lstvArchive.Name = "lstvArchive";
+            lstvArchive.Size = new Size(495, 414);
+            lstvArchive.TabIndex = 1;
+            lstvArchive.UseCompatibleStateImageBehavior = false;
+            lstvArchive.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Name";
+            columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Date";
+            columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Type";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Size";
+            columnHeader4.Width = 100;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Peru;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstvArchive);
+            Controls.Add(btnOpen);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnOpen;
+        private ListView lstvArchive;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,24 @@
+namespace FileListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return;
+            }
+            
+
+
+
+
+        }
+    }
+}
